fc5d2cd452a03aeb48fda42b0edf48b2f2c4bab0
Ibnu Mas'ud <ibnumasud.gemail@gmail.com>
2023-02-18 21:35:58 +0700

    update request

--- server.go
+++ server.go
@@ -10,6 +10,7 @@ func main() {
 	e := echo.New()
 	e.GET("/", getHelloWorld)
 	e.GET("/user/:id", getUser)
+	e.POST("/users", postUsers)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
@@ -20,4 +21,18 @@ func getHelloWorld(c echo.Context) error {
 func getUser(c echo.Context) error {
 	id := c.Param("id")
 	return c.String(http.StatusOK, id)
+}
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
+func postUsers(c echo.Context) error {
+	u := new(User)
+	//Assignment statement, err nya di assign disini aja
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.XML(http.StatusOK, u)
 }
