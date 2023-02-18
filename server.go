b5d65ff082f57f1b941c8b4b10c31d3bd758c9f8
Ibnu Mas'ud <ibnumasud.gemail@gmail.com>
2023-02-18 12:55:10 +0700

    first init

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", getHelloWorld)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getHelloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello Ibnu dari Echo")
+}
